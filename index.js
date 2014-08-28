d231618f9d19c4e86de369e51e6b9e15f5a9160c
Marcel Erz <marcel.erz@gmail.com>
2014-08-28 00:31:50 -0700

    Cleanup and consistency; add additional commands

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 'use strict';
 
 var util = require('util');
-var Browser = require('./lib/browser.js');
 
-module.exports = getBrowser;
+var Alert = require('lib/alert');
+var Browser = require('lib/browser');
+var Cookie = require('lib/cookie');
+var CookieStorage = require('lib/cookieStorage');
+var Element = require('lib/element');
+var Frame = require('lib/frame');
+var GlobalMouse = require('lib/globalMouse');
+var GlobalTouch = require('lib/globalTouch');
+var IME = require('lib/ime');
+var Keys = require('lib/keys');
+var LocalStorage = require('lib/localStorage');
+var LogEntry = require('lib/logEntry');
+var Mouse = require('lib/mouse');
+var Navigator = require('lib/navigator');
+var Session = require('lib/session');
+var SessionStorage = require('lib/sessionStorage');
+var Status = require('lib/status');
+var TimeOut = require('lib/timeOut');
+var Touch = require('lib/touch');
+var WindowHandler = require('lib/window');
+
+module.exports = Cabbie;
 
 /**
  * Create a new browser session, remember to call `.dispose()`
  * at the end to terminate the session.
  *
  * @param {String|Object} remote
- * @param {Object}        capabilities
- * @param {Object}        options
+ * @param {Object} capabilities
+ * @param {Object} options
  */
-function getBrowser(remote, capabilities, options) {
+function Cabbie(remote, capabilities, options) {
   var browser = new Browser(remote, capabilities, options);
+
   if (options.debug) {
     browser.on('method-call', function (event) {
       var msg = event.target;
@@ -33,3 +54,26 @@ function getBrowser(remote, capabilities, options) {
   }
   return browser;
 }
+
+Cabbie.Alert = Alert;
+Cabbie.Browser = Browser;
+Cabbie.Cookie = Cookie;
+Cabbie.CookieStorage = CookieStorage;
+Cabbie.Element = Element;
+Cabbie.Frame = Frame;
+Cabbie.GlobalMouse = GlobalMouse;
+Cabbie.GlobalTouch = GlobalTouch;
+Cabbie.IME = IME;
+Cabbie.Keys = Keys;
+Cabbie.LocalStorage = LocalStorage;
+Cabbie.LogEntry = LogEntry;
+Cabbie.Mouse = Mouse;
+Cabbie.Navigator = Navigator;
+Cabbie.Session = Session;
+Cabbie.SessionStorage = SessionStorage;
+Cabbie.Status = Status;
+Cabbie.TimeOut = TimeOut;
+Cabbie.Touch = Touch;
+Cabbie.WindowHandler = WindowHandler;
+
+

e2526e5e0f349600ef06a2feeb883c3c66abdb99
Marcel Erz <marcel.erz@gmail.com>
2014-09-09 22:15:50 -0700

    Bugfixes

--- index.js
+++ index.js
@@ -2,28 +2,28 @@
 
 var util = require('util');
 
-var Alert = require('lib/alert');
-var Browser = require('lib/browser');
-var Cookie = require('lib/cookie');
-var CookieStorage = require('lib/cookieStorage');
-var Element = require('lib/element');
-var Frame = require('lib/frame');
-var GlobalMouse = require('lib/globalMouse');
-var GlobalTouch = require('lib/globalTouch');
-var IME = require('lib/ime');
-var Keys = require('lib/keys');
-var LocalStorage = require('lib/localStorage');
-var LogEntry = require('lib/logEntry');
-var Mouse = require('lib/mouse');
-var Navigator = require('lib/navigator');
-var Session = require('lib/session');
-var SessionStorage = require('lib/sessionStorage');
-var Status = require('lib/status');
-var TimeOut = require('lib/timeOut');
-var Touch = require('lib/touch');
-var WindowHandler = require('lib/window');
+var Alert = require('./lib/alert');
+var Browser = require('./lib/browser');
+var Cookie = require('./lib/cookie');
+var CookieStorage = require('./lib/cookieStorage');
+var Element = require('./lib/element');
+var Frame = require('./lib/frame');
+var GlobalMouse = require('./lib/globalMouse');
+var GlobalTouch = require('./lib/globalTouch');
+var IME = require('./lib/ime');
+var Keys = require('./lib/keys');
+var LocalStorage = require('./lib/localStorage');
+var LogEntry = require('./lib/logEntry');
+var Mouse = require('./lib/mouse');
+var Navigator = require('./lib/navigator');
+var Session = require('./lib/session');
+var SessionStorage = require('./lib/sessionStorage');
+var Status = require('./lib/status');
+var TimeOut = require('./lib/timeOut');
+var Touch = require('./lib/touch');
+var WindowHandler = require('./lib/window');
 
-module.exports = Cabbie;
+module.exports = cabbie;
 
 /**
  * Create a new browser session, remember to call `.dispose()`
@@ -33,12 +33,32 @@ module.exports = Cabbie;
  * @param {Object} capabilities
  * @param {Object} options
  */
-function Cabbie(remote, capabilities, options) {
-  var browser = new Browser(remote, capabilities, options);
+function cabbie(remote, capabilities, options) {
+  var browser = new Browser(remote, capabilities, options),
+      indentation = 0;
+
+  function stringFill (filler, length) {
+    var buffer = new Buffer(length);
+    buffer.fill(filler);
+    return buffer.toString();
+  }
+
+  function getIndentation (add) {
+    return stringFill(' ', (indentation + add) * 2);
+  }
 
   if (options.debug) {
+    if (options.httpDebug) {
+      browser.on('request', function (req) {
+        console.log(getIndentation(1) + "Request:  ", JSON.stringify(req).substr(0, 5000));
+      });
+      browser.on('response', function (res) {
+        console.log(getIndentation(1) + "Response: ", JSON.stringify(res).substr(0, 5000));
+      });
+    }
     browser.on('method-call', function (event) {
       var msg = event.target;
+      indentation = event.indentation;
       if (event.selector) {
         msg += '(' + util.inspect(event.selector, {colors: true}) + ')';
       }
@@ -49,31 +69,34 @@ function Cabbie(remote, capabilities, options) {
       if (event.result && typeof event.result !== 'object') {
         msg += ' => ' + util.inspect(event.result, {colors: true});
       }
-      console.log('     - ' + msg);
+      console.log(getIndentation(0) + '[' + (event.state + stringFill(' ', 5)).substr(0, 5) + '] ' + msg);
+      if (event.state.toLowerCase() !== 'start') {
+        console.log(getIndentation(0) + stringFill('-', 50));
+      }
     });
   }
   return browser;
 }
 
-Cabbie.Alert = Alert;
-Cabbie.Browser = Browser;
-Cabbie.Cookie = Cookie;
-Cabbie.CookieStorage = CookieStorage;
-Cabbie.Element = Element;
-Cabbie.Frame = Frame;
-Cabbie.GlobalMouse = GlobalMouse;
-Cabbie.GlobalTouch = GlobalTouch;
-Cabbie.IME = IME;
-Cabbie.Keys = Keys;
-Cabbie.LocalStorage = LocalStorage;
-Cabbie.LogEntry = LogEntry;
-Cabbie.Mouse = Mouse;
-Cabbie.Navigator = Navigator;
-Cabbie.Session = Session;
-Cabbie.SessionStorage = SessionStorage;
-Cabbie.Status = Status;
-Cabbie.TimeOut = TimeOut;
-Cabbie.Touch = Touch;
-Cabbie.WindowHandler = WindowHandler;
+cabbie.Alert = Alert;
+cabbie.Browser = Browser;
+cabbie.Cookie = Cookie;
+cabbie.CookieStorage = CookieStorage;
+cabbie.Element = Element;
+cabbie.Frame = Frame;
+cabbie.GlobalMouse = GlobalMouse;
+cabbie.GlobalTouch = GlobalTouch;
+cabbie.IME = IME;
+cabbie.Keys = Keys;
+cabbie.LocalStorage = LocalStorage;
+cabbie.LogEntry = LogEntry;
+cabbie.Mouse = Mouse;
+cabbie.Navigator = Navigator;
+cabbie.Session = Session;
+cabbie.SessionStorage = SessionStorage;
+cabbie.Status = Status;
+cabbie.TimeOut = TimeOut;
+cabbie.Touch = Touch;
+cabbie.WindowHandler = WindowHandler;
 
 

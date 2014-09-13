767f8e37f8837d8f80a56b0ae545529ab293f58f
Marcel Erz <marcel.erz@gmail.com>
2014-09-12 23:35:40 -0700

    Add documentation

---
+++ docs/api.js
@@ -0,0 +1,76 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ActiveWindow",
+        "Alert",
+        "Browser",
+        "Cabbie",
+        "Connection",
+        "Cookie",
+        "CookieStorage",
+        "Driver",
+        "Element",
+        "Frame",
+        "GlobalMouse",
+        "GlobalTouch",
+        "IME",
+        "Keys",
+        "LocalStorage",
+        "LogEntry",
+        "Mouse",
+        "Navigator",
+        "Session",
+        "SessionStorage",
+        "Status",
+        "TimeOut",
+        "Touch",
+        "WindowHandler"
+    ],
+    "modules": [
+        "Cabbie",
+        "Core",
+        "Interaction",
+        "Navigation",
+        "Storage",
+        "System",
+        "WebDriver"
+    ],
+    "allModules": [
+        {
+            "displayName": "Cabbie",
+            "name": "Cabbie",
+            "description": "Create a new browser session\n\n<img src=\"../../objectReference.png\" />\n\nNote: Remember to call `.dispose()` at the end to terminate the session."
+        },
+        {
+            "displayName": "Core",
+            "name": "Core",
+            "description": "Core components of the system"
+        },
+        {
+            "displayName": "Interaction",
+            "name": "Interaction",
+            "description": "Peripheral interactions"
+        },
+        {
+            "displayName": "Navigation",
+            "name": "Navigation",
+            "description": "Navigation classes"
+        },
+        {
+            "displayName": "Storage",
+            "name": "Storage",
+            "description": "Storage related classes"
+        },
+        {
+            "displayName": "System",
+            "name": "System",
+            "description": "System classes - internals"
+        },
+        {
+            "displayName": "WebDriver",
+            "name": "WebDriver",
+            "description": "Web-Driver objects"
+        }
+    ]
+} };
+});

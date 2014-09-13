f3b0a8d1f03c9a65259c73b10bf84d3f1172cfc0
Marcel Erz <marcel.erz@gmail.com>
2014-09-12 23:36:15 -0700

    Complete configuration

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 var util = require('util');
 
 var Alert = require('./lib/alert');
+var ActiveWindow = require('./lib/activeWindow');
 var Browser = require('./lib/browser');
 var Cookie = require('./lib/cookie');
 var CookieStorage = require('./lib/cookieStorage');
+var Driver = require('./lib/driver');
 var Element = require('./lib/element');
 var Frame = require('./lib/frame');
 var GlobalMouse = require('./lib/globalMouse');
@@ -23,18 +25,29 @@ var TimeOut = require('./lib/timeOut');
 var Touch = require('./lib/touch');
 var WindowHandler = require('./lib/window');
 
-module.exports = cabbie;
+module.exports = Cabbie;
 
 /**
- * Create a new browser session, remember to call `.dispose()`
- * at the end to terminate the session.
+ * Create a new browser session
  *
- * @param {String|Object} remote
- * @param {Object} capabilities
+ * <img src="../../objectReference.png" />
+ *
+ * Note: Remember to call `.dispose()` at the end to terminate the session.
+ *
+ * @constructor
+ * @class Cabbie
+ * @module Cabbie
+ * @param {String|Object} remote Request object or URL to selenium-server
+ * @param {Object} capabilities See capabilities in {{#crossLink "Session"}}{{/crossLink}}
  * @param {Object} options
+ * @param {String} options.mode Mode of web-driver requests (Browser.MODE_SYNC|Browser.MODE_ASYNC)
+ * @param {String} [options.base] Base-url
+ * @param {String} [options.sessionID]
+ * @param {Boolean} [options.debug=false]
+ * @param {Boolean} [options.httpDebug=false]
  */
-function cabbie(remote, capabilities, options) {
-  var browser = new Browser(remote, capabilities, options),
+function Cabbie(remote, capabilities, options) {
+  var driver = new Driver(remote, capabilities, options),
       indentation = 0;
 
   function stringFill (filler, length) {
@@ -49,14 +62,14 @@ function cabbie(remote, capabilities, options) {
 
   if (options.debug) {
     if (options.httpDebug) {
-      browser.on('request', function (req) {
+      driver.on('request', function (req) {
         console.log(getIndentation(1) + "Request:  ", JSON.stringify(req).substr(0, 5000));
       });
-      browser.on('response', function (res) {
+      driver.on('response', function (res) {
         console.log(getIndentation(1) + "Response: ", JSON.stringify(res).substr(0, 5000));
       });
     }
-    browser.on('method-call', function (event) {
+    driver.on('method-call', function (event) {
       var msg = event.target;
       indentation = event.indentation;
       if (event.selector) {
@@ -75,28 +88,138 @@ function cabbie(remote, capabilities, options) {
       }
     });
   }
-  return browser;
+  return driver;
 }
 
-cabbie.Alert = Alert;
-cabbie.Browser = Browser;
-cabbie.Cookie = Cookie;
-cabbie.CookieStorage = CookieStorage;
-cabbie.Element = Element;
-cabbie.Frame = Frame;
-cabbie.GlobalMouse = GlobalMouse;
-cabbie.GlobalTouch = GlobalTouch;
-cabbie.IME = IME;
-cabbie.Keys = Keys;
-cabbie.LocalStorage = LocalStorage;
-cabbie.LogEntry = LogEntry;
-cabbie.Mouse = Mouse;
-cabbie.Navigator = Navigator;
-cabbie.Session = Session;
-cabbie.SessionStorage = SessionStorage;
-cabbie.Status = Status;
-cabbie.TimeOut = TimeOut;
-cabbie.Touch = Touch;
-cabbie.WindowHandler = WindowHandler;
 
+/**
+ * @property ActiveWindow
+ * @type {ActiveWindow}
+ */
+Cabbie.ActiveWindow = ActiveWindow;
 
+/**
+ * @property Alert
+ * @type {Alert}
+ */
+Cabbie.Alert = Alert;
+
+/**
+ * @property Browser
+ * @type Browser
+ */
+Cabbie.Browser = Browser;
+
+/**
+ * @property Cookie
+ * @type Cookie
+ */
+Cabbie.Cookie = Cookie;
+
+/**
+ * @property CookieStorage
+ * @type CookieStorage
+ */
+Cabbie.CookieStorage = CookieStorage;
+
+/**
+ * @property Driver
+ * @type Driver
+ */
+Cabbie.Driver = Driver;
+
+/**
+ * @property Element
+ * @type Element
+ */
+Cabbie.Element = Element;
+
+/**
+ * @property Frame
+ * @type Frame
+ */
+Cabbie.Frame = Frame;
+
+/**
+ * @property GlobalMouse
+ * @type GlobalMouse
+ */
+Cabbie.GlobalMouse = GlobalMouse;
+
+/**
+ * @property GlobalTouch
+ * @type GlobalTouch
+ */
+Cabbie.GlobalTouch = GlobalTouch;
+
+/**
+ * @property IME
+ * @type IME
+ */
+Cabbie.IME = IME;
+
+/**
+ * @property Keys
+ * @type Keys
+ */
+Cabbie.Keys = Keys;
+
+/**
+ * @property LocalStorage
+ * @type LocalStorage
+ */
+Cabbie.LocalStorage = LocalStorage;
+
+/**
+ * @property LogEntry
+ * @type LogEntry
+ */
+Cabbie.LogEntry = LogEntry;
+
+/**
+ * @property Mouse
+ * @type Mouse
+ */
+Cabbie.Mouse = Mouse;
+
+/**
+ * @property Navigator
+ * @type Navigator
+ */
+Cabbie.Navigator = Navigator;
+
+/**
+ * @property Session
+ * @type Session
+ */
+Cabbie.Session = Session;
+
+/**
+ * @property SessionStorage
+ * @type SessionStorage
+ */
+Cabbie.SessionStorage = SessionStorage;
+
+/**
+ * @property Status
+ * @type Status
+ */
+Cabbie.Status = Status;
+
+/**
+ * @property TimeOut
+ * @type TimeOut
+ */
+Cabbie.TimeOut = TimeOut;
+
+/**
+ * @property Touch
+ * @type Touch
+ */
+Cabbie.Touch = Touch;
+
+/**
+ * @property WindowHandler
+ * @type WindowHandler
+ */
+Cabbie.WindowHandler = WindowHandler;

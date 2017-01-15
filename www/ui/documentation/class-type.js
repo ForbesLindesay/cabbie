156f658b6a6a4e4df3281fb8c397a2f61dda02bd
Forbes Lindesay <forbes@lindesay.co.uk>
2017-01-15 05:31:48 +0000

    prettify

--- www/ui/documentation/class-type.js
+++ www/ui/documentation/class-type.js
@@ -5,10 +5,12 @@ import ImportDeclaration from './import-declaration';
 import CodeBlock from './code-block';
 
 function ClassType({classType}) {
-  return <section>
-    <h1>{classType.name}</h1>
-    <pre>{JSON.stringify(classType, null, '  ')}</pre>
-  </section>;
+  return (
+    <section>
+      <h1>{classType.name}</h1>
+      <pre>{JSON.stringify(classType, null, '  ')}</pre>
+    </section>
+  );
 }
 
 export default ClassType;
